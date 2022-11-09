7c3641f5f116e292b623ef05bd098d58e82f744b
Alex <alex1.0@hotmail.es>
2022-11-09 23:57:53 +0100

    Cerrojos

--- Pena Capital/Assets/Scripts/OpenLockedDoor.cs
+++ Pena Capital/Assets/Scripts/OpenLockedDoor.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class OpenLockedDoor : MonoBehaviour
 {
+    private bool doorOpened;
+    private bool coroutineAllowed;
+    private float a;
+    private float b;
+    private float c;
+    private InventoryManager IM;
+    public float g;
+    public string comprobar;
+
+    //public GameObject doorWing; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorOpened = false;
+        coroutineAllowed = true;
+        a=transform.localRotation.eulerAngles.x;
+        b=transform.localRotation.eulerAngles.y;
+        c=transform.localRotation.eulerAngles.z;
+        IM = GameObject.FindGameObjectWithTag("InventoryManager").GetComponent<InventoryManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Invoke("RunCoroutine", 0f);
     }
-}
+
+    private void RunCoroutine()
+    {
+        if (coroutineAllowed)
+            {StartCoroutine("OpenThatDoor");}
+    }
