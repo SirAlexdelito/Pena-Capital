576c4dabb8222f37b8b5f959f258a9be73a28bc5
Alex <alex1.0@hotmail.es>
2022-12-13 19:09:45 +0100

    Comic y textos

--- Pena Capital/Assets/Scripts/Tutorial.cs
+++ Pena Capital/Assets/Scripts/Tutorial.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,52 +7,77 @@ public class Tutorial : MonoBehaviour
 {
     int fase;
     bool corroutine;
+    bool onw8;
     void Start()
     {
-        DisplayText.Instance.changeTextTuto("Muevete con W A S D");
+        DisplayText.Instance.changeTextTuto("");
+
         fase = 0;
-        corroutine=true;
+        corroutine = true;
+        onw8 = false;
     }
     void Update()
     {
         if (fase == 0 && (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")))
         {
-            DisplayText.Instance.changeTextTuto("Prueba a coger un objeto de la mesa con click izquierdo");
-            fase++;
-            DisplayText.Instance.onTuto=false;
+            try
+            {
+                GameObject.FindGameObjectWithTag("Comic").SetActive(false);
+                DisplayText.Instance.changeTextTuto("Muevete con W A S D");
+
+                StartCoroutine("w8");
+            }
+            catch (Exception) { }
         }
-        else if(fase==1 && InventoryManager.Instance.Items.Count>0){
-            DisplayText.Instance.onTuto=true;
+        if (fase == 1 && (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")))
+        {
+            if (!onw8)
+            {
+                DisplayText.Instance.changeTextTuto("Prueba a coger un objeto de la mesa con click izquierdo");
+
+                StartCoroutine("w8");
+
+
+            }
+        }
+        else if (fase == 2 && InventoryManager.Instance.Items.Count > 0)
+        {
+            DisplayText.Instance.onTuto = true;
             DisplayText.Instance.changeTextTuto("El inventario se abre con la i ");
             fase++;
         }
-        else if(fase==1 && InventoryManager.Instance.Items.Count>0){
-            DisplayText.Instance.changeTextTuto("El inventario se abre con la i ");
-            fase++;
-        }
-        else if (fase == 2 && Input.GetButtonDown("OpenInventory"))
+        else if (fase == 3 && Input.GetButtonDown("OpenInventory"))
         {
             DisplayText.Instance.changeTextTuto("Para seleccionar un objeto haz click en su icono");
             fase++;
         }
-        else if (fase == 3 && InventoryManager.Instance.SelectedContent.childCount==1)
+        else if (fase == 4 && InventoryManager.Instance.SelectedContent.childCount >= 1)
         {
-            if(!corroutine)return;
+            if (!corroutine) return;
             StartCoroutine("LastTuto");
         }
     }
     IEnumerator LastTuto()
     {
-        corroutine=false;
-        DisplayText.Instance.changeTextTuto("Al interactuar con el nivel lo harás con el objeto que tengas seleccionado");
+        corroutine = false;
+        DisplayText.Instance.changeTextTuto("Al interactuar con el nivel lo haras con el objeto que tengas seleccionado");
         yield return new WaitForSeconds(3f);
         DisplayText.Instance.changeTextTuto("");
         yield return new WaitForSeconds(1f);
-        DisplayText.Instance.Text.color=Color.red;
+        DisplayText.Instance.Text.color = Color.red;
         DisplayText.Instance.changeTextTuto("Debo encontrar la salida...");
-        DisplayText.Instance.Text.color=Color.white;
+        DisplayText.Instance.Text.color = Color.white;
         yield return new WaitForSeconds(4f);
-        DisplayText.Instance.onTuto=false;
+        DisplayText.Instance.onTuto = false;
         Destroy(this.gameObject);
+    }
+    IEnumerator w8()
+    {
+        onw8 = true;
+        yield return new WaitForSeconds(3f);
+        if (fase==1)
+            DisplayText.Instance.onTuto = false;
+        onw8 = false;
+        fase++;
     }
 }
